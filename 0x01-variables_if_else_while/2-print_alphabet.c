01a13c3b3f4dbcd0bcea3e8ad7bea3f2ad5ee3ff
root <ncwilliams111@gmail.com>
2023-08-14 21:34:05 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main functin
+ * Return: zero as always
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main fuction begins
+ * Return: zero as expected
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'Z' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
+}
