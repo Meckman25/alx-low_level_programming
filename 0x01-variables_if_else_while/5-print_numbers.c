a5cb33eba00e0af854f76d680c46695b411d46b2
root <ncwilliams111@gmail.com>
2023-08-14 22:07:02 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function begins here
+ * Return: zero most often
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 10 ; i++)
+		printf("%d", i);
+	return (0);
+}
