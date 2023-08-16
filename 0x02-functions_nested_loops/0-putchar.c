07757bbd3877c720c0bf0cad72793c6640a025b2
root <ncwilliams111@gmail.com>
2023-08-15 18:05:16 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - This is the entry point of the function
+ *
+ * Return: Always 0 on success
+ */
+
+int main(void)
+{
+
+printf("_putchar\n");
+
+return (0);
+}
+
